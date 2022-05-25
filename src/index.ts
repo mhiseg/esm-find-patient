04dffac2afff71bebbb9bbcbba605ed9af3509e9
casimirMorency <morencycasimir@gmail.com>
2022-05-25 18:22:49 -0400

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import { getAsyncLifecycle, defineConfigSchema } from "@openmrs/esm-framework";
+import { configSchema } from "./config-schema";
+
+const importTranslation = require.context(
+  "../translations",
+  false,
+  /.json$/,
+  "lazy"
+);
+
+const backendDependencies = {
+  fhir2: "^1.2.0",
+  "webservices.rest": "^2.2.0",
+};
+
+function setupOpenMRS() {
+  const moduleName = "@mhiseg/esm-find-patient-app";
+
+  const options = {
+    featureName: "find-patient",
+    moduleName,
+  };
+
+  defineConfigSchema(moduleName, configSchema);
+
+  return {
+    pages: [
+      {
+        load: getAsyncLifecycle(
+          () => import("./searchPatient/searchPatient"),
+          options
+        ),
+        route: "death/search",
+      },
+    ],
+    extensions: [{}],
+  };
+}
+
+export { backendDependencies, importTranslation, setupOpenMRS };
